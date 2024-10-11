cde93657fd636d9913e33eddac63794a27aea409
ElenaIva <ei492493@gmail.com>
2024-10-11 13:46:41 +0200

    Add files via upload

---
+++ module2_2.py
@@ -0,0 +1,11 @@
+first = input("Введите 1 число:")
+second = input("Введите 2 число:")
+third = input("Введите 3 число:")
+if first == second and second == third:
+    print(3)
+elif first == second or first == third:
+        print(2)
+else:
+        print(0)
+
+
